d1713bc1c2108770aabf67de68b5a923a1bf0be9
Arseny Sokolov <me@arsen.pw>
2020-01-09 14:14:56 +0800

    Initial bot.

---
+++ handler.py
@@ -0,0 +1,65 @@
+import json
+import telegram
+import os
+import logging
+
+# Logging is cool!
+logger = logging.getLogger()
+if logger.handlers:
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logging.basicConfig(level=logging.INFO)
+
+OK_RESPONSE = {
+    'statusCode': 200,
+    'header': {'Content-Type': 'application/json'},
+    'body': json.dumps('ok')
+}
+ERROR_RESPONSE = {
+    'statusCode': 400,
+    'body': json.dumps('Oops, something went wrong!')
+}
+
+
+def configure_telegram():
+    TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
+    if not TELEGRAM_TOKEN:
+        logger.error('The TELEGRAM_TOKEN must be set')
+        raise NotImplementedError
+    return telegram.Bot(TELEGRAM_TOKEN)
+
+
+def webhook(event, context):
+    bot = configure_telegram()
+    logger.info('Event: {}'.format(event))
+
+    if event.get('httpMethod') == 'POST' and event.get('body'):
+        logger.info('Message received')
+        update = telegram.Update.de_json(json.loads(event.get('body')), bot)
+        chat_id = update.message.chat.id
+        text = update.message.text
+
+        if text == '/start':
+            text = 'Приветствую, друг мой, напиши с какого города ты, пиши по русски и полное название города.'
+
+        bot.sendMessage(chat_id=chat_id, text=text)
+        logger.info('Message sent')
+
+        return OK_RESPONSE
+
+    return ERROR_RESPONSE
+
+
+def set_webhook(event, context):
+    logger.info('Event: {}'.format(event))
+    bot = configure_telegram()
+    url = 'https://{}/{}'.format(
+        event.get('headers').get('Host'),
+        event.get('requestContext').get('stage'),
+    )
+    webhook = bot.set_webhook(url)
+
+    if webhook:
+        return OK_RESPONSE
+
+    return ERROR_RESPONSE
